d92fe9e68528625adfa4bf6e18e9e0a80f6d28a1
rodrigoo-r <rodrigo@zelixlang.dev>
2025-10-24 23:49:24 -0500

    Add lightweight exception types for Celery library

--- include/celery/except/base.h
+++ include/celery/except/base.h
@@ -18,16 +18,47 @@
 #pragma once
 #include <exception>
 
+/**
+ * @namespace Celery::Except
+ * @brief Exception types used across the Celery library.
+ *
+ * This namespace contains lightweight exception types that mirror
+ * the behavior of standard C++ exceptions but are specific to
+ * Celery's error reporting.
+ */
 namespace Celery::Except
 {
+    /**
+     * @class Exception
+     * @brief A minimal exception wrapper around a C-style message.
+     *
+     * This class derives from std::exception and holds a pointer to a
+     * null-terminated C string describing the error. The message pointer
+     * is not copied by this class; callers must ensure the lifetime of
+     * the provided string outlives the exception object.
+     */
     class Exception : public std::exception
     {
         const char *msg;
 
     public:
+        /**
+         * @brief Construct an Exception with a message.
+         * @param msg A null-terminated C string describing the error.
+         *
+         * Note: The string is not owned by this object. Keep the string
+         * valid for the lifetime of the exception.
+         */
         Exception(const char *msg) : msg(msg)
         {}
 
+        /**
+         * @brief Return the explanatory string.
+         * @return The message provided at construction.
+         *
+         * This overrides std::exception::what() and is guaranteed to be
+         * noexcept.
+         */
         [[nodiscard]] const char *what()
         const noexcept override
         {

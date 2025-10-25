7b97d467010f3c5add7d8fb99a2840fd77196564
rodrigoo-r <rodrigo@zelixlang.dev>
2025-10-25 00:01:49 -0500

    Add Stdout class for platform-specific output handling

---
+++ include/celery/stdout.h
@@ -0,0 +1,47 @@
+/*
+                    --- Celery ---
+    Celery is a high-performance C++ STL-like
+    library that provides efficient data structures
+    and algorithms for modern applications.
+
+    It is distributed under the Apache 2.0 License,
+    if you do not have a copy of the license,
+    you can find it at:
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Please see the LICENSE file for more information,
+    as individuals are to follow the terms of the license
+    as part of a binding agreement. Govern yourself
+    accordingly.
+*/
+
+#pragma once
+#include <cstddef>
+#include <iostream>
+
+// Guard against malware operating systems
+#ifndef _WIN32
+    // Unix-like system headers
+#   include <unistd.h>
+#else
+    // For losers
+#   include <iostream>
+#endif
+
+namespace Celery::Io
+{
+    class Stdout
+    {
+        static inline void Write(const char *data, size_t size)
+        {
+#           ifndef _WIN32
+                // Normal person:
+                write(STDOUT_FILENO, data, size);
+#           else
+                // Mentally disabled person who uses
+                // malware as their operating system:
+                std::cout.write(data, size);
+#           endif
+        }
+    };
+}

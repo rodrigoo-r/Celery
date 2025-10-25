6d7c517a2f12107d2d69c20f1cecf0bc41a11db8
rodrigoo-r <rodrigo@zelixlang.dev>
2025-10-24 23:52:31 -0500

    Add SystemAllocator class for memory management in Celery

---
+++ include/celery/memory/system.h
@@ -0,0 +1,39 @@
+/*
+                    --- Celery ---
+    Celery is a high-performance C++ STL-like
+    library that provides efficient data structures
+    and algorithms for modern applications.
+
+    It is distributed under the Apache 2.0 License,
+    if you do not have a copy of the license,
+    you can find it at:
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Please see the LICENSE file for more information,
+    as individuals are to follow the terms of the license
+    as part of a binding agreement. Govern yourself
+    accordingly.
+*/
+
+#pragma once
+#include "base.h"
+
+namespace Celery::Pmr
+{
+    template <typename T>
+    class SystemAllocator : Allocator<T>
+    {
+        template <typename ...Args>
+        static inline T *Allocate(Args &&...args)
+        {
+            // Create a raw ptr
+            T *ptr = static_cast<T *>(::operator new(sizeof(T)));
+
+            // Use placement new to construct the object
+            new (ptr) T(std::forward<Args>(args)...);
+
+            // Return the constructed object
+            return ptr;
+        }
+    };
+}

a48d31512e9867148fb563a763005e77816764f6
rodrigoo-r <rodrigo@zelixlang.dev>
2025-10-25 00:06:56 -0500

    Add SystemArrayAllocator class for array memory management

--- include/celery/memory/system.h
+++ include/celery/memory/system.h
@@ -23,6 +23,7 @@ namespace Celery::Pmr
     template <typename T>
     class SystemAllocator : Allocator<T>
     {
+    public:
         /**
          * @brief Allocate raw memory for a single object of type T and construct it.
          *
@@ -65,6 +66,44 @@ namespace Celery::Pmr
             }
 
             ::operator delete(ptr); // Free the memory
+        }
+    };
+
+    template <typename T>
+    class SystemArrayAllocator : ArrayAllocator<T>
+    {
+    public:
+        /**
+         * @brief Allocate raw memory for an array of T objects.
+         *
+         * Allocates enough uninitialized memory to hold \p count objects of type T
+         * using the global ::operator new. This function does not construct the
+         * individual objects; callers must construct elements using placement new.
+         *
+         * @param count Number of T objects to allocate memory for.
+         * @return Pointer to uninitialized memory capable of holding \p count T objects.
+         * @throws std::bad_alloc if allocation fails.
+         */
+        static inline T *Allocate(const size_t count)
+        {
+            // Allocate raw memory for count T objects
+            T *ptr = static_cast<T *>(::operator new(sizeof(T) * count));
+            return ptr;
+        }
+
+        /**
+         * @brief Deallocate memory previously obtained from Allocate.
+         *
+         * Releases memory allocated by Allocate using the global ::operator delete.
+         * This function does not call destructors for objects stored in the memory;
+         * callers must explicitly destroy objects (e.g., by calling their destructors)
+         * before invoking Deallocate. Passing a null pointer is safe.
+         *
+         * @param ptr Pointer to memory returned by Allocate (may be nullptr).
+         */
+        static inline void Deallocate(T *ptr)
+        {
+            ::operator delete(ptr);
         }
     };
 }

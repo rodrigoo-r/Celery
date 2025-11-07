032c0d09531c9e5a2eea7a2aa69fe6bd3207070b
rodrigoo-r <rodrigo@zelixlang.dev>
2025-11-07 18:02:51 -0500

    Add Display specialization for Celery::Str::String to support raw output

---
+++ include/celery/serialize/string.h
@@ -0,0 +1,43 @@
+/*
+                    --- Celery ---
+    Celery is a high-performance C++ STL-like
+    library that provides efficient data structures
+    and algorithms for modern applications.
+
+    It is distributed under the Apache 2.0 License,
+    if you do not have a copy of the license,
+    you can find it at:
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Please see the LICENSE file for more information,
+    as individuals are to follow the terms of the license
+    as part of a binding agreement. Govern yourself
+    accordingly.
+*/
+
+#pragma once
+#include "displayable.h"
+
+namespace Celery::Serialize
+{
+    /**
+     * @brief Specialization of Display for Celery::Str::String type.
+     *
+     * This specialization provides methods to output Celery::Str::String
+     * in raw form to an output stream.
+     */
+    template<>
+    struct Display<Str::String>
+    {
+        /*
+         *  @brief Output a raw representation of a Celery::Str::String to the provided stream.
+         *
+         * @param obj The Celery::Str::String to output.
+         * @param stream The output stream to write to.
+         */
+        static inline void Raw(Str::String &obj, Io::Pmr::OStream<> &stream)
+        {
+            stream.Batch(obj.Ptr(), obj.Len());
+        }
+    };
+}

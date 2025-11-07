86b4639dced5deb3fe8b7383ba29f060aa2738df
rodrigoo-r <rodrigo@zelixlang.dev>
2025-11-07 17:21:42 -0500

    Refactor build scripts for improved clarity and organization

--- build-prod.sh
+++ build-prod.sh
@@ -2,4 +2,5 @@
 # Build script for the Celery project using CMake.
 # ============================================================================ #
 
-cmake -DCMAKE_BUILD_TYPE=Release --build cmake-build-debug --target Celery -j 8
+cmake -DCMAKE_BUILD_TYPE=Release -S . -B cmake-build-release
+cmake --build cmake-build-release --target Celery -j 8

bb059f5568d7c3a8c4f0379f23976a973e848fe4
rodrigoo-r <rodrigo@zelixlang.dev>
2025-11-07 09:46:44 -0500

    Enhance Comparable concept with detailed documentation and improve type checking

--- include/celery/base/comparable.h
+++ include/celery/base/comparable.h
@@ -20,12 +20,30 @@
 
 namespace Celery::Base
 {
+    /**
+     * @brief Primary template for Compare struct.
+     *
+     * Specializations of this struct should provide a callable
+     * operator() that takes an instance of T and returns a size_t
+     * representing the comparison result.
+     *
+     * @tparam T The type to be compared.
+     */
     template <typename T>
     struct Compare;
 
+    /**
+     * @brief Concept to check if a type T has a valid Compare specialization.
+     *
+     * A type T satisfies the Comparable concept if there exists a
+     * Compare<T> specialization that can be invoked with an instance
+     * of T and returns a size_t convertible result.
+     *
+     * @tparam T The type to be checked for comparability.
+     */
     template <typename T>
-    concept Comparable = requires(T a, T b)
-    {
-        { Compare<T>{}(a, b) } -> std::convertible_to<bool>;
+    concept Comparable = requires(T&& t) {
+            { Compare<std::remove_cvref_t<T>>{}(std::forward<T>(t)) }
+            -> std::convertible_to<std::size_t>;
     };
 }

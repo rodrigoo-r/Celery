0e1b038b9461b69702429b6632e9bd3440097cbf
rodrigoo-r <rodrigo@zelixlang.dev>
2025-11-07 21:31:04 -0500

    Refactor target_include_directories to improve interface visibility for project dependencies

--- cmake/link.cmake
+++ cmake/link.cmake
@@ -7,7 +7,13 @@ add_library(xxhash STATIC ${xxhash_SOURCE_DIR}/xxhash.c)
 target_include_directories(xxhash PUBLIC ${xxhash_SOURCE_DIR})
 
 # Link libraries
-target_include_directories(${PROJECT_NAME} PRIVATE include ${xxhash_SOURCE_DIR})
+target_include_directories(${PROJECT_NAME}
+        PUBLIC
+        $<BUILD_INTERFACE:${CMAKE_CURRENT_SOURCE_DIR}/include>
+        $<INSTALL_INTERFACE:include>
+        PRIVATE
+        ${xxhash_SOURCE_DIR}
+)
 target_link_libraries(${PROJECT_NAME}
         PRIVATE
         xxhash

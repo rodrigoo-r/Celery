e6fa17679e33e7c01ee2bb6fc72bea3553747709
rodrigoo-r <rodrigo@zelixlang.dev>
2025-11-07 21:26:59 -0500

    Remove unused MyClass implementation and related code from main.cpp

--- main.cpp
+++ main.cpp
@@ -1,31 +1,3 @@
-#include "celery/io/output.h"
-#include "celery/ptr/shared.h"
-#include "celery/ptr/unique.h"
-
-struct MyClass
-    : Celery::Base::Display
-{
-    [[nodiscard]] Celery::Str::String ToString()
-        const noexcept override
-    {
-        return "MyClass(2)";
-    }
-};
-
-void copy(Celery::Ptr::Shared<MyClass> c)
-{
-
-}
-
-int main()
-{
-    auto a = Celery::Ptr::MakeShared<MyClass>();
-    auto b = Celery::Ptr::MakeUnique<MyClass>();
-    auto d = Celery::Ptr::MakeConcurrent<MyClass>();
-    Celery::Ptr::Shared<MyClass> c = a;
-    copy(a);
-
-    Celery::Io::Println("Obj: ", *a);
-    Celery::Io::Println("Obj: ", *b);
-    return 0;
-}
+// Placeholder file so the compiler and syntax analysis
+// in IDEs do not complain about missing files.
+// Please keep empty.

2d34b9b38022a525bb1d89fcd93e30dfb6248bcd
rodrigoo-r <rodrigo@zelixlang.dev>
2025-11-07 19:03:21 -0500

    Refactor Raw method in string.h to use universal references for improved flexibility

--- include/celery/serialize/string.h
+++ include/celery/serialize/string.h
@@ -35,7 +35,7 @@ namespace Celery::Serialize
          * @param obj The Celery::Str::String to output.
          * @param stream The output stream to write to.
          */
-        static inline void Raw(Str::String &obj, Io::Pmr::OStream<> &stream)
+        static inline void Raw(Str::String &&obj, Io::Pmr::OStream<> &stream)
         {
             stream.Batch(obj.Ptr(), obj.Len());
         }

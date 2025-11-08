0eeeb2af4e1d3e7aeb8d7dbb35cf3598b0839eef
rodrigoo-r <rodrigo@zelixlang.dev>
2025-11-07 19:30:47 -0500

    Refactor Raw method to use template parameter for improved type safety

--- include/celery/serialize/string.h
+++ include/celery/serialize/string.h
@@ -35,7 +35,11 @@ namespace Celery::Serialize
          * @param obj The Celery::Str::String to output.
          * @param stream The output stream to write to.
          */
-        static inline void Raw(Str::String &&obj, Io::Pmr::OStream<> &stream)
+        template<
+            class U = Str::String,
+            typename = Trait::EnsureSame<U, Str::String>
+        >
+        static inline void Raw(U &&obj, Io::Pmr::OStream<> &stream)
         {
             stream.Batch(obj.Ptr(), obj.Len());
         }

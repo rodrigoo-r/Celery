dd4ccbec42263416fcb70ed8207504f06ff1ece9
rodrigoo-r <rodrigo@zelixlang.dev>
2025-11-07 19:25:09 -0500

    Add default.h header file with library information and serialization includes

---
+++ include/celery/serialize/default.h
@@ -0,0 +1,25 @@
+/*
+                    --- Celery ---
+    Celery is a high-performance C++ STL-like
+    library that provides efficient data structures
+    and algorithms for modern applications.
+
+    It is distributed under the Apache 2.0 License,
+    if you do not have a copy of the license,
+    you can find it at:
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Please see the LICENSE file for more information,
+    as individuals are to follow the terms of the license
+    as part of a binding agreement. Govern yourself
+    accordingly.
+*/
+
+#pragma once
+#include "celery/serialize/char.h"
+#include "celery/serialize/cstr.h"
+#include "celery/serialize/string.h"
+#include "celery/serialize/external.h"
+#include "celery/serialize/bool.h"
+#include "celery/serialize/float.h"
+#include "celery/serialize/integral.h"

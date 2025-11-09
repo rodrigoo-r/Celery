6e06d9607ae73753928e29c5c65226168d1084da
rodrigoo-r <rodrigo@zelixlang.dev>
2025-11-09 10:15:55 -0500

    Add Optional class for managing optional values

---
+++ include/celery/misc/optional.h
@@ -0,0 +1,184 @@
+/*
+                    --- Celery ---
+    Celery is a high-performance C++ STL-like
+    library that provides efficient data structures
+    and algorithms for modern applications.
+
+    It is distributed under the Apache 2.0 License,
+    if you do not have a copy of the license,
+    you can find it at:
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Please see the LICENSE file for more information,
+    as individuals are to follow the terms of the license
+    as part of a binding agreement. Govern yourself
+    accordingly.
+*/
+
+#pragma once
+#include "celery/except/out_of_range.h"
+#include "celery/trait/type.h"
+
+namespace Celery::Misc
+{
+    /**
+     * @brief A simple optional container that may or may not hold a value of type T.
+     *
+     * This class provides storage for an object of type T using
+     * placement new and tracks whether a value is currently
+     * engaged (present) or not. It supports construction,
+     * assignment, and access to the contained value.
+     *
+     * @tparam T The type of the optional value.
+     */
+    template <typename T>
+    class Optional
+    {
+    protected:
+        alignas (T) unsigned char storage[sizeof(T)];
+        bool engaged = false;
+
+    public:
+        /**
+         * @brief Default constructor creates an empty Optional.
+         */
+        Optional() = default;
+
+        /**
+         * @brief Constructs an Optional with the given value.
+         *
+         * Uses placement new to construct the value in the aligned storage.
+         *
+         * @tparam U Type of the value to store (must be the same as T).
+         * @param value The value to store in the Optional.
+         */
+        template <
+            typename U = T,
+            typename = Trait::EnsureSame<U, T>
+        >
+        Optional(U &&value)
+        {
+            new (storage) T(std::forward<U>(value));
+            engaged = true;
+        }
+
+        /**
+         * @brief Copy constructor.
+         *
+         * If the other Optional is engaged, copies its value into this one.
+         *
+         * @param other The other Optional to copy from.
+         */
+        Optional(const Optional &other)
+        {
+            if (other.engaged)
+            {
+                new (storage) T(other.Value());
+                engaged = true;
+            }
+        }
+
+        /**
+         * @brief Move constructor.
+         *
+         * If the other Optional is engaged, moves its value into this one
+         * and disengages the other.
+         *
+         * @param other The other Optional to move from.
+         */
+        Optional(Optional &&other) noexcept
+        {
+            if (other.engaged)
+            {
+                new (storage) T(std::move(other.Value()));
+                engaged = true;
+                other.engaged = false;
+            }
+        }
+
+        /**
+         * @brief Destructor.
+         *
+         * If engaged, calls the destructor of the contained value.
+         */
+        ~Optional()
+        {
+            if (engaged)
+            {
+                reinterpret_cast<T *>(storage)->~T();
+            }
+        }
+
+        /**
+         * @brief Assignment operator.
+         *
+         * If this Optional is already engaged, destroys the current value
+         * before constructing the new value in place.
+         *
+         * @tparam U Type of the value to assign (must be the same as T).
+         * @param other The value to assign to this Optional.
+         * @return Reference to this Optional after assignment.
+         */
+        template <
+            typename U = T,
+            typename = Trait::EnsureSame<U, T>
+        >
+        Optional &operator=(U &&other)
+        {
+            if (engaged)
+            {
+                reinterpret_cast<T *>(storage)->~T();
+            }
+
+            new (storage) T(std::forward<U>(other));
+            engaged = true;
+            return *this;
+        }
+
+        /**
+         * @brief Checks if the Optional currently holds a value.
+         *
+         * @return true if a value is present, false otherwise.
+         */
+        [[nodiscard]] bool HasValue() const
+        {
+            return engaged;
+        }
+
+        /**
+         * @brief Accessor for the contained value.
+         *
+         * Throws Except::OutOfRange if no value is present.
+         *
+         * @return Reference to the contained value.
+         * @throws Except::OutOfRange if no value is present.
+         */
+        T &Value()
+        {
+            if (!engaged)
+            {
+                throw Except::OutOfRange();
+            }
+
+            return *reinterpret_cast<T *>(storage);
+        }
+
+        /**
+         * @brief Member access operator for the contained value.
+         *
+         * Throws Except::OutOfRange if no value is present.
+         *
+         * @return Pointer to the contained value.
+         * @throws Except::OutOfRange if no value is present.
+         */
+        T *operator->()
+        {
+            if (!engaged)
+            {
+                throw Except::OutOfRange();
+            }
+
+            return reinterpret_cast<T *>(storage);
+        }
+    };
+} // namespace Celery::Misc

ba93d6dc1bef3e1164febe40ba451978ad5e2e9f
rodrigoo-r <rodrigo@zelixlang.dev>
2025-11-11 10:43:20 -0500

    Refactor link.cmake to remove xxHash library references and update target_include_directories to use INTERFACE

--- cmake/link.cmake
+++ cmake/link.cmake
@@ -3,14 +3,8 @@
 # ============================================================================ #
 
 # Link libraries
-target_include_directories(${PROJECT_NAME}
-        PUBLIC
+target_include_directories(
+        Celery INTERFACE
         $<BUILD_INTERFACE:${CMAKE_CURRENT_SOURCE_DIR}/include>
         $<INSTALL_INTERFACE:include>
-        PRIVATE
-        ${xxhash_SOURCE_DIR}
-)
-target_link_libraries(${PROJECT_NAME}
-        PRIVATE
-        xxhash
 )

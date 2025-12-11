7e0e43c6c4fe13daa9e494a342628d2e54004acb
rodrigoo-r <rodrigo@zelixlang.dev>
2025-12-11 17:13:41 -0500

    Add StringEquality struct: Implement a utility for comparing string-like objects

---
+++ include/celery/misc/string_equal.h
@@ -0,0 +1,55 @@
+/*
+                    --- Celery ---
+    Celery is a high-performance C++ STL-like
+    library that provides efficient data structures
+    and algorithms for modern applications.
+
+    It is distributed under the Apache 2.0 License,
+    if you do not have a copy of the license,
+    you can find it at:
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Please see the LICENSE file for more information,
+    as individuals are to follow the terms of the license
+    as part of a binding agreement. Govern yourself
+    accordingly.
+*/
+
+#include <celery/trait/default.h>
+#include <celery/trait/type.h>
+#include <celery/string/string.h>
+#include <celery/string/external.h>
+
+namespace Celery::Misc
+{
+    struct StringEquality
+    {
+        using is_transparent = void;
+
+        template <
+            typename T,
+            typename U,
+            typename = std::enable_if<
+                Trait::EnsureSameBase<
+                    T, Celery::Str::String
+                > ||
+                Trait::EnsureSameBase<
+                    T, Celery::Str::External
+                >
+            >,
+            typename = std::enable_if<
+                Trait::EnsureSameBase<
+                    U, Celery::Str::String
+                > ||
+                Trait::EnsureSameBase<
+                    U, Celery::Str::External
+                >
+            >
+        >
+        bool operator()(T &&a, U &&b) const noexcept
+        {
+            return a.Size() == b.Size() &&
+                   memcmp(a.Ptr(), b.Ptr(), a.Size()) == 0;
+        }
+    }
+}

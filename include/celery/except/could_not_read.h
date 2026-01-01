49005204fb101f762d56ad20d857b755128a4355
rodrigoo-r <rodrigo@zelixlang.dev>
2026-01-01 11:39:30 -0500

    Add CouldNotRead exception class for error handling: Implement a new exception class to handle read errors from files or sockets, enhancing error reporting in the Celery library.

---
+++ include/celery/except/could_not_read.h
@@ -0,0 +1,52 @@
+/*
+                    --- Celery ---
+    Celery is a high-performance C++ STL-like
+    library that provides efficient data structures
+    and algorithms for modern applications.
+
+    It is distributed under the Apache 2.0 License,
+    if you do not have a copy of the license,
+    you can find it at:
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Please see the LICENSE file for more information,
+    as individuals are to follow the terms of the license
+    as part of a binding agreement. Govern yourself
+    accordingly.
+*/
+
+#pragma once
+#include "base.h"
+
+/**
+ * @namespace Celery::Except
+ * @brief Contains exception types used across the Celery library.
+ *
+ * Exceptions in this namespace provide consistent error reporting
+ * for various error conditions encountered by Celery components.
+ */
+namespace Celery::Except
+{
+    /**
+     * @class CouldNotRead
+     * @brief Exception thrown when a file or socket cannot be read.
+     *
+     * This exception indicates that an attempt to read from a file
+     * or socket has failed, possibly due to I/O errors, permission
+     * issues, or other underlying problems.
+     */
+    class CouldNotRead : public Exception
+    {
+    public:
+        /**
+         * @brief Constructs a CouldNotRead exception with a default message.
+         */
+        CouldNotRead()
+            : Exception("Could not read a file or socket.")
+        {}
+
+        CouldNotRead(Celery::Str::External &details)
+            : Exception(details)
+        {}
+    };
+}
